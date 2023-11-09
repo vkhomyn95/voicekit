586ef59c96616149e7d6a28b09120a893e35bd1c
vkhomyn <vkhomyn95@gmail.com>
2023-11-09 09:56:55 +0200

    Test commit not valid

--- asterisk_modules/app_grpcsttbackground/grpc_stt.cpp
+++ asterisk_modules/app_grpcsttbackground/grpc_stt.cpp
@@ -544,8 +544,9 @@ bool GRPCSTT::Run(int &error_status, std::string &error_message)
 						voiptime::cloud::stt::v1::StreamingRecognizeRequest request;
 						std::vector<uint8_t> buffer = make_silence_samples(frame_format, gap_samples);
 						request.set_audio_content(buffer.data(), buffer.size());
-						if (!stream->Write(request))
-							stream_valid = false;
+						stream->Write(request)
+//						if (!stream->Write(request))
+//							stream_valid = false;
 						time_add_samples(&last_frame_moment, gap_samples);
 					}
 //					continue;
@@ -570,8 +571,9 @@ bool GRPCSTT::Run(int &error_status, std::string &error_message)
 								voiptime::cloud::stt::v1::StreamingRecognizeRequest request;
 								std::vector<uint8_t> buffer = make_silence_samples(frame_format, gap_samples);
 								request.set_audio_content(buffer.data(), buffer.size());
-								if (!stream->Write(request))
-									stream_valid = false;
+								stream->Write(request)
+//								if (!stream->Write(request))
+//									stream_valid = false;
 								time_add_samples(&last_frame_moment, gap_samples);
 							}
 							gap_handled = true;
@@ -584,8 +586,9 @@ bool GRPCSTT::Run(int &error_status, std::string &error_message)
 						if (data) {
 							time_add_samples(&last_frame_moment, f->samples);
 							request.set_audio_content(data, len);
-							if (!stream->Write(request))
-                                stream_valid = false;
+							stream->Write(request)
+//							if (!stream->Write(request))
+//                                stream_valid = false;
 						}
 					}
 
